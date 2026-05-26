831f9fe91fc2f8bcf57d7c48487f3a1226b145b4
Happy_Diogenes <tibu10@naver.com>
2026-05-26 13:06:33 +0900

    0525 월
    무기 조준 로직 구현

--- Assets/Scripts/Player/WeaponController.cs
+++ Assets/Scripts/Player/WeaponController.cs
@@ -1,3 +1,4 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
@@ -6,6 +7,38 @@ public class WeaponController : MonoBehaviour
 
     void Update()
     {
-        
+        FindClosestEnemy();
+
+        if(target != null)
+        {
+            RotateToTarget();
+        }
+    }
+
+    void FindClosestEnemy()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); // 적 객체 배열
+
+        float closestDistance = Mathf.Infinity; // 초기값 = 무한
+        Transform closestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+
+            if(distance < closestDistance) // 최소 거리 = 저장
+            {
+                closestDistance = distance;
+                closestEnemy = enemy.transform;
+            }
+        }
+        target = closestEnemy;
+    }
+
+    void RotateToTarget()
+    {
+        Vector2 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,angle);
     }
 }
